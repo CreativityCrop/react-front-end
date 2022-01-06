2e09524d2cf0777d9cd974744be5e0eba3b1ab77
urtohadthover <zorry.artinova@gmail.com>
2022-01-06 13:58:26 +0200

    somewhat finished the first section of the homepage
    its very ugly ik, but will be fixed soon

--- src/homepage/FirstSection.js
+++ src/homepage/FirstSection.js
@@ -1,16 +1,16 @@
 export default function FirstSection(){
     return(
-        <div className="pt-40 select-none">
+        <div className="pt-28 select-none">
                 <div id="holder" className="flex flex-row">
-                    <div className="bg-violet-600 grid gird-cols-6 text-center p-10 mr-20">
-                        <h1 className="text-4xl tetx-extrabold mb-2">A marketplace for</h1>
+                    <div className="grid gird-cols-6 text-center p-8 mr-20 ml-5 mt-8 w-auto h-56">
+                        <h1 className="text-5xl tetx-extrabold mb-2">A marketplace for</h1>
                         <div className="max-w-30 content-center">
-                            <h2 className="text-4xl text-extrabold mb-6 py-2 hover:bg-slate-300">ideas</h2>
+                            <h2 className="text-5xl text-extrabold mb-3 py-2 hover:text-red-600">ideas</h2>
                         </div>
-                        <h3 className="text-xl">insparational quote here</h3>
+                        <h3 className="text-2xl">insparational quote here</h3>
                     </div>
-                    <div className="bg-violet-600 grid gird-cols-6 p-10 ml-20">
-                        <p>this is a cool picture, imagine it</p>
+                    <div className="bg-violet-600 grid gird-cols-6 p-10 w-96 h-72">
+                        {/* <p>this is a cool picture, imagine it</p> */}
                     </div>
                 </div>
         </div>
