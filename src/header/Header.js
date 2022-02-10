cc4b4344814b929ee7c7476ff13c39db0c3003d2
Georgi Iliev <georgi.iliev533@outlook.com>
2022-02-10 20:50:56 +0200

    Header Fix smaller screens

--- src/header/Header.js
+++ src/header/Header.js
@@ -14,9 +14,9 @@ export default function Header() {
 
     return (
         <div id="header"
-        className="flex flex-row flex-wrap py-2 px-10 sm:pl-0 border-b-2 justify-between place-items-center select-none">
-            <Logo className="w-44 md:w-40 sm:m-auto"/>
-            <button className="hidden sm:block w-12 h-12" onClick={() => setOpened(prevOpened => !prevOpened)}><HamburgerSVG/></button>
+        className="flex flex-row flex-wrap py-2 xl:px-10 px-4 border-b-2 justify-between place-items-center select-none">
+            <Logo className="w-44 md:w-40 ml-3 mr-auto shrink-0"/>
+            <button className="hidden sm:block shrink-0 mr-3 w-12 h-12" onClick={() => setOpened(prevOpened => !prevOpened)}><HamburgerSVG/></button>
             <LinkMenu className="sm:m-auto" opened={opened}/>
         </div>
     );
