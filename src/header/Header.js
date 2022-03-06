6b81ef0df9941e2c3fe602b1a19ffe2e41809d29
Georgi Iliev <georgi.iliev533@outlook.com>
2022-03-06 19:31:07 +0200

    Font preloading

--- src/header/Header.js
+++ src/header/Header.js
@@ -18,7 +18,7 @@ export default function Header() {
         className="flex flex-row flex-wrap py-2 xl:px-10 px-4 border-b-2 justify-between place-items-center select-none">
             <div className="sm:w-full flex flex-row justify-between place-items-center">
                 <Logo className="w-44 md:w-40 ml-3 shrink-0"/>
-                <button className="hidden sm:block shrink-0 mr-3 w-12 h-12" onClick={() => setOpened(prevOpened => !prevOpened)}><HamburgerSVG/></button>
+                <button className="hidden sm:block shrink-0 mr-3 w-12 h-12" onClick={() => setOpened(prevOpened => !prevOpened)}><HamburgerSVG alt="hamburger menu" /></button>
             </div>
             <LinkMenu className="sm:m-auto" opened={opened}/>
         </div>
